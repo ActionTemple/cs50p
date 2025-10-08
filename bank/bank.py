7fc036d97df52a4a37c1d7c4a5de8f02720a9a2e
ActionTemple <andr3wwaddington@gmail.com>
2025-10-08 12:14:30 +0400

    Add bank folder from CS50.dev

---
+++ bank/bank.py
@@ -0,0 +1,16 @@
+
+
+# Bank
+# Andrew Waddington
+
+greeting = input("Greeting: ").lower().strip()
+
+if greeting.startswith("hello"):
+    print ("$0")
+
+# check if first letter of the word starts with "h", and if so print $20
+elif greeting.startswith("h"):
+    print ("$20")
+else:
+    print ("$100")
+
